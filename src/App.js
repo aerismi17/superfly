c74cad10552b4f7468aad5bafc03d8f6c9ea36b1
Erica Smith <ericasmith@Ericas-MacBook-Pro.local>
2021-03-15 20:44:24 -0700

    main image credits to https://dribbble.com/AOKURA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
+  //state
+  const[city, setCity] = useState('')
+  const[weather, setWeather] = useState({})
+  //api key and url
+
+  const apiKey = process.env.REACT_APP_API_KEY
+  //const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  async function getWeather(){
+    let url = 'https://api.openweathermap.org/data/2.5/weather'
+    url += '?q=' + city
+    url += '&appid=' + apiKey
+    const r = await fetch(url)
+    const j = await r.json()
+    if(j.weather) {
+      setWeather(j.weather)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Nunito+Sans"></link>
+      <div className = 'header'>
+        <div className = 'links'>
+          <div className = 'logo'/>
+          <a class = 'active' href = '#home'>SUPERFLY</a>
+          <a href= '#discord'>DISCORD</a>
+          <a href= '#team'>TEAM</a>
+          <a href= '#leaderboard'>LEADERBOARD</a>
+          <a href= '#contact'>CONTACT</a>
+        </div>
+
+        <div className = 'weather'>
+          YOUR CITY: 
+          <TextField id = 'city' variant="standard" color = 'secondary' style = {{width: '100px'}}
+            value = {city} onChange = {e=> setCity(e.target.value)} autoFocus
+            onKeyPress = {e => e.key=== 'Enter' && getWeather()}
+          />
+          {weather && weather.length > 0 && <div className = 'weatherIcon'>
+            {weather && weather.map(m=> {
+              const img = m.icon
+              return <img src = {`http://openweathermap.org/img/w/${img}.png`} alt = '' />
+            })} 
+          </div>}
+        </div>
+      </div>
+
+      <div className = 'main'>
+        <p className = 'tagline'>Be a hero. Save your city.</p>
+        <div className = 'brand'>SUPERFLY</div>
+        <div className = 'container'>
+          <a className = 'playNowButton' href= "#storelink">PLAY NOW</a>
+        </div>
+      </div>
+
     </div>
   );
 }
